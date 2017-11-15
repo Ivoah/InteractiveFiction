87b95cec071fab63b70606b16d0a08a86b791f90
Noah Rosamilia <ivoahivoah@gmail.com>
2017-11-14 20:44:43 -0500

    Simplify verb functions

--- InteractiveFiction/Verbs.py
+++ InteractiveFiction/Verbs.py
@@ -7,8 +7,7 @@ def get(world, target):
 
     print(f'You cannot find {ana(target)}.')
 
-def take(world, *args):
-    target = args[0]
+def take(world, target):
     item = get(world, target)
     if item is None: return
     if item in world.player.inventory:
@@ -18,8 +17,7 @@ def take(world, *args):
     world.location.items.remove(item)
     world.player.inventory.append(item)
 
-def drop(world, *args):
-    target = args[0]
+def drop(world, target):
     item = get(world, target)
     if item is None: return
     if item not in world.player.inventory:
@@ -29,45 +27,42 @@ def drop(world, *args):
     world.player.inventory.remove(item)
     world.location.items.append(item)
 
-def inventory(world, *args):
+def inventory(world):
     print('You are carrying:')
     for item in world.player.inventory:
         if item.shown:
             print(f'    {item.name}')
 
-def north(world, *args):
+def north(world):
     go(world, 'north')
 
-def south(world, *args):
+def south(world):
     go(world, 'south')
 
-def east(world, *args):
+def east(world):
     go(world, 'east')
 
-def west(world, *args):
+def west(world):
     go(world, 'west')
 
-def go(world, *args):
-    direction = args[0]
+def go(world, direction):
     if hasattr(world.location, direction) and getattr(world.location, direction):
         world.location = world[getattr(world.location, direction)]
         look(world)
     else:
         print('You cannot go that way.')
 
-def look(world, *args):
+def look(world):
     print(world.location.__doc__)
     for item in world.location.items:
         item.see()
 
-def examine(world, *args):
-    target = args[0]
+def examine(world, target):
     item = get(world, target)
     if item is None: return
     print(item.__doc__)
 
-def test(world, *args):
-    test = args[0]
+def test(world, test):
     if test in world.tests:
         for line in world.tests[test]:
             print(f'> {line}')
@@ -75,8 +70,7 @@ def test(world, *args):
     else:
         print(f'Test "{test}" not found.')
 
-def quit(world, *args):
-    yes = input('Are you sure you want to quit? ')
+def quit(world, yes = ''):
     while yes not in ['yes', 'no']:
         print('Please answer "yes" or "no".')
         yes = input('Are you sure you want to quit? ')
